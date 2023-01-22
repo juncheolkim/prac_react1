a18856105243c66e66f327b8f17fe12ca00b21af
김준철 <kimjoon889@gamil.com>
2023-01-22 15:33:06 +0900

    react-route-dom 6버전 이후부터는 useHistory가 없어졌다. 대신, useNavigate가 생겼다.
    const navigate = useNavigate();
    
    function onAddMeetupHandler(meetupData) {
        fetch(
            "https://react-getting-started-174ef-default-rtdb.firebaseio.com/meetups.json",
            {
                method: "POST",
                body: JSON.stringify(meetupData),
                headers: {
                    "Content-Type": "application/json",
                },
            }
        ).then(() => {
            navigate("/", { replace: true });
        });
    }
    
    이처럼 useNavigate 의 return값을 상수에 넣고, fetch 의 promise 객체를 동기적으로 기다린다.
    (.then은 promise객체에서 제공하는 메서드.)
    then을 사용하는 이유는 fetch가 해당 데이터의 POST작업을 끝날때까지 기다렸다 실행하기 위해서.
    이후 navigate는 '/' 로 이동하며, redirect를 하기 위해서 replace:true 를 두번째 인자로 전달한다.
    redirect를 하는 이유는 react는 기본적으로 하나의 HTML에서 처음에 받는 데이터로 컴포넌트를 구성하기 때문에,
    새로운 데이터로 컴포넌트를 구성하기 위해서 서버에 다시 해당 컴포넌트를 요청하는 것으로 보자.
    (뇌피셜임.)
    
    	modified:   src/components/meetups/NewMeetupForm.js
    	modified:   src/pages/NewMeetup.js

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,29 @@
+import { useNavigate } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+    const navigate = useNavigate();
+
+    function onAddMeetupHandler(meetupData) {
+        fetch(
+            "https://react-getting-started-174ef-default-rtdb.firebaseio.com/meetups.json",
+            {
+                method: "POST",
+                body: JSON.stringify(meetupData),
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        ).then(() => {
+            navigate("/", { replace: true });
+        });
+    }
+
     return (
         <section>
             <h1>Add New Meetup</h1>
-            <NewMeetupForm />
+            <NewMeetupForm onAddMeetup={onAddMeetupHandler} />
         </section>
     );
 }
