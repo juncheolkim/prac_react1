76046b6d32dccbb271b432c6244a45bb0dadf164
김준철 <kimjoon889@gamil.com>
2023-01-22 14:53:15 +0900

    Form 데이터를 주고 받기 위해 리액트가 처음에 할 일. 1. preventHandler로 submit event를 차단한다. 2. 사용자 입력값을 가져온다 (useRef)
    	modified:   src/components/meetups/NewMeetupForm.js

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,24 +1,64 @@
+import { useRef } from "react";
+
 import Card from "../ui/Card";
 import calsses from "./NewMeetupForm.module.css";
 function NewMeetupForm() {
+    const titleInputRef = useRef();
+    const imageInputRef = useRef();
+    const addressInputRef = useRef();
+    const descriptionInputRef = useRef();
+
+    function submitHandler(event) {
+        event.preventDefault();
+
+        const enteredTitle = titleInputRef.current.value;
+        const enteredImage = imageInputRef.current.value;
+        const enteredAddress = addressInputRef.current.value;
+        const enteredDescription = descriptionInputRef.current.value;
+
+        const meetupData = {
+            title: enteredTitle,
+            image: enteredImage,
+            address: enteredAddress,
+            description: enteredDescription,
+        };
+
+        console.log(meetupData);
+    }
+
     return (
         <Card>
-            <form className={calsses.form}>
+            <form className={calsses.form} onSubmit={submitHandler}>
                 <div className={calsses.control}>
                     <label htmlFor="title">Meetup Title</label>
-                    <input type="text" required id="title" />
+                    <input
+                        type="text"
+                        required
+                        id="title"
+                        ref={titleInputRef}
+                    />
                 </div>
                 <div className={calsses.control}>
                     <label htmlFor="image">Meetup Image</label>
-                    <input type="url" required id="image" />
+                    <input type="url" required id="image" ref={imageInputRef} />
                 </div>
                 <div className={calsses.control}>
                     <label htmlFor="address">Address</label>
-                    <input type="url" required id="address" />
+                    <input
+                        type="text"
+                        required
+                        id="address"
+                        ref={addressInputRef}
+                    />
                 </div>
                 <div className={calsses.control}>
                     <label htmlFor="description">Description</label>
-                    <textarea id="description" required rows="5" />
+                    <textarea
+                        id="description"
+                        required
+                        rows="5"
+                        ref={descriptionInputRef}
+                    />
                 </div>
                 <div className={calsses.action}>
                     <button>Add Meetup</button>
